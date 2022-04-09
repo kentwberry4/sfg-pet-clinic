9589d799c2137d7c122347345d895a80c09518f1
kentwberry4 <kentwberry@protonmail.com>
2022-04-09 18:14:58 -0400

    adding service interfaces

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.models.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save (Vet owner);
+
+    Set<Vet> findAll();
+}
